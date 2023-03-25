85597e5102c026c27f1b1ea552cd1296f836fd3e
kenkai21 <engrkenokafor@gmail.com>
2023-03-25 14:18:40 +0100

    authentication feature

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,10 +1,8 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
-
-import { verify, decode } from 'jsonwebtoken'
+import { verify} from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
@@ -69,7 +67,6 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
     logger.error('Fail to authenticate the user', err)
   }
 }
-
 function getToken(authHeader: string): string {
   if (!authHeader) throw new Error('No authentication header')
 
